381598e6e12c41919422af9cd253b37b634a7fc0
Kizal <sanket.jivtode@gmail.com>
2025-12-17 02:04:46 +0530

    Delete submit_assignment.bat

--- submit_assignment.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-git remote remove origin
-git remote add origin https://github.com/Kizal/01-fullstack-vendor-onboarding.git
-git checkout -b submission
-git add .
-git commit -m "Complete assignment: Frontend polish, Delete vendor, Bug fix, Unique email, Docker"
-git push -u origin submission
